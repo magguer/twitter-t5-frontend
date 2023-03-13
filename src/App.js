53772206f409544dacd52666240143caab8916a6
magguer <martintelaguer@gmail.com>
2023-03-13 13:32:52 -0300

    data-back-in-front
    npm i axios
    axios to users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect } from "react"
 
 function App() {
+  //const [getInfo, setGetInfo] = 
+
+  useEffect(() => {
+    const getFilmInfo = async () => {
+      const response = await axios.get(
+        "http://localhost:8000/users"
+      );
+      console.log(response);
+    };
+    getFilmInfo();
+  }, []);
+
   return (
     <div className="App">
-
     </div>
   );
 }
