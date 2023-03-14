fdcd0a59925c279ecdc3bc7c40c3b503e62a6a10
Emi <emi.lng711@gmail.com>
2023-03-14 14:50:15 -0300

    followers and following

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,5 +1,67 @@
 function Followers() {
-  return <div>Followers</div>;
+  return (
+    <>
+      <div className="row">
+        <div className="col-2 border p-0">
+          {/* <%- include("../partials/navbar") %> */}
+        </div>
+        {/* <!--       Body --> */}
+        <div className="col-10 col-lg-7 p-0">
+          <div className="px-3 pt-3">
+            {/* <!--           Flecha Atrás --> */}
+            <div className="d-flex align-items-center mt-4">
+              <a
+                className="text-decoration-none text-black fs-4"
+                href="/usuarios/<%= userFollowers.username %>"
+              >
+                🡠
+              </a>
+              <div className="d-flex align-items-start flex-column ms-4">
+                <h5 className="mb-0">
+                  {/* <%= userFollowers.firstname %> <%= userFollowers.lastname %> */}
+                </h5>
+                <small>{/* @<%= userFollowers.username %> */}</small>
+              </div>
+            </div>
+          </div>
+          <div className="mt-3 d-flex justify-content-around">
+            <div>
+              <a className="fs-5 text-black fw-semibold text-decoration-none">
+                Followers
+              </a>
+              <div
+                style={{
+                  background: "#1d9bf0",
+                  width: "100%",
+                  height: "4px",
+                  marginTop: "8px",
+                }}
+              ></div>
+            </div>
+            <div>
+              <a
+                href="following"
+                className="fs-5 text-black fw-semibold text-decoration-none"
+              >
+                Following
+              </a>
+            </div>
+          </div>
+          <div className="d-flex flex-column border-top pt-4">
+            {/* <% for (const smallUser of users) { %>
+          <!-- comment -->
+          <%- include("../partials/smallUser", { smallUser, globalUser }) %>
+          <!-- comment -->
+          <%}%> */}
+          </div>
+        </div>
+        {/* <!--       More info --> */}
+        <div className="col-4 d-none d-lg-block col-lg-3 border p-0">
+          {/* <%- include("../partials/moreinfo", { usersInfo, globalUser }) %> */}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Followers;
--- src/components/Following.jsx
+++ src/components/Following.jsx
@@ -1,5 +1,70 @@
 function Following() {
-  return <div>Following</div>;
+  return (
+    <>
+      <div className="row">
+        {/* <!--       NavBar --> */}
+        <div className="col-2 border p-0">
+          {/* <%- include("../partials/navbar") %> */}
+        </div>
+        {/* <!--       Body --> */}
+        <div className="col-10 col-lg-7 p-0">
+          <div className="px-3 pt-3">
+            {/* <!--           Flecha Atrás --> */}
+            <div className="d-flex align-items-center mt-4">
+              <a
+                className="text-decoration-none text-black fs-4"
+                href="/usuarios/<%= userFollowing.username %>"
+              >
+                🡠
+              </a>
+              <div className="d-flex align-items-start flex-column ms-4">
+                <h5 className="mb-0">
+                  {/* <%= userFollowing.firstname %> <%= userFollowing.lastname %> */}
+                </h5>
+                <small>{/* @<%= userFollowing.username %> */}</small>
+              </div>
+            </div>
+          </div>
+          <div className="mt-3 d-flex justify-content-around">
+            <div>
+              <a
+                href="followers"
+                className="fs-5 text-black fw-semibold text-decoration-none"
+              >
+                Followers
+              </a>
+            </div>
+            <div>
+              <a className="fs-5 text-black fw-semibold text-decoration-none">
+                Following
+              </a>
+              <div
+                style={{
+                  background: "#1d9bf0",
+                  width: "100%",
+                  height: "4px",
+                  marginTop: "8px",
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className="d-flex flex-column border-top pt-4">
+            {/* <!-- TO DO: Hacer que entre a el arreglo following dentro del Modelo User --> */}
+            {/* <% for (const smallUser of users){%>
+          <!-- comment -->
+          <!-- comment -->
+          <%- include("../partials/smallUser", { smallUser, globalUser }) %>
+          <!-- comment -->
+          <%}%> */}
+          </div>
+        </div>
+        {/* <!--       More info --> */}
+        <div className="col-4 d-none d-lg-block col-lg-3 border p-0">
+          {/* <%- include("../partials/moreinfo", { usersInfo, globalUser }) %> */}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Following;
