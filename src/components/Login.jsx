8741807beceb4c494d3faf4866ebe902a474ee0e
magguer <martintelaguer@gmail.com>
2023-03-14 14:49:43 -0300

    login-comp-add-react

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,43 +1,109 @@
+// Dependencias
+import { useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// Slice
 import { login } from "../redux/userSlice";
 
 function Login() {
-  //const [getInfo, setGetInfo] =
-  const [email, setEmail] = useState("Blanca_Ortega@gmail.com");
-  const [password, setPassword] = useState("asd");
-  const [userToken, setUserToken] = useState("");
-  const [users, setUsers] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const getToken = async () => {
-      const response = await axios.post("http://localhost:8000/token", {
-        email: email,
-        password: password,
-      });
-      const user = response.data;
-      setUserToken(user.token);
-      dispatch(login(user));
-    };
-    getToken();
-  }, []);
-
-  const getUsers = async () => {
-    const response = await axios({
-      headers: { Authorization: `Bearer ${userToken}` },
-      method: "get",
-      url: "http://localhost:8000/users",
+  const getToken = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:8000/token", {
+      email,
+      password,
     });
-    setUsers(response.data);
+    const user = response.data;
+    dispatch(login(user));
+    navigate("/");
   };
 
   return (
-    // Pasar la view de Login y ver la logica que tenia en los <%= %>
-    <div>
-      <button className="btn btn-primary">Boton</button>
+    <div
+      className="text-emphasis-primary"
+      style={{ backgroundColor: "#065990" }}
+    >
+      <div className="container">
+        <div
+          className="row g-0 rounded overflow-hidden shadow mt-5"
+          style={{ height: "80vh" }}
+        >
+          <div
+            className="container d-none col-lg-8 col-md-4 text-white d-md-flex align-items-start flex-column"
+            style={{ backgroundColor: "#1d9bf0" }}
+          >
+            <div className="col ms-3 mt-3">
+              <img
+                className="mt-3"
+                style={{ width: "3rem", height: "3rem" }}
+                src="https://firebasestorage.googleapis.com/v0/b/noboss-app.appspot.com/o/ha.dev%2FTeam5-TwitterApp%2Flogo-twitter-white.svg?alt=media&token=386c4a48-1183-4cc7-8b66-8ad36ec10dc4"
+                alt="Twitter"
+              />
+            </div>
+            <div className="col d-flex align-items-end ms-3 mb-3">
+              <strong>
+                <p className="fs-5">Hi! Nice to see you again 🥰</p>
+              </strong>
+            </div>
+          </div>
+          <div className="col-12 col-lg-4 col-md-8 bg-white text-dark d-flex align-items-center justify-content-center">
+            <form onSubmit={getToken}>
+              <div className="mt-5 mb-3">
+                <h2 className="fw-bold">Login</h2>
+                <small className="fw-semibold">
+                  Ready to start using Twitter?
+                </small>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="name@example.com"
+                />
+                <label htmlFor="floatingInput">Email</label>
+              </div>
+              <div className="form-floating">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <label htmlFor="floatingPassword">Password</label>
+              </div>
+              <div className="d-grid gap-1 my-5">
+                <button
+                  className="btn text-light"
+                  style={{ borderRadius: "45px", backgroundColor: "#1d9bf0" }}
+                  type="submit"
+                >
+                  Login
+                </button>
+              </div>
+              <div className="my-5 text-center">
+                <a
+                  href="/register"
+                  className="text-decoration-none text-dark fw-semibold"
+                  role="button"
+                >
+                  Don't have an account? Sign Up.
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
