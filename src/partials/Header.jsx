752912bd182f131211fa2d1599084d57d210ac70
Emi <emi.lng711@gmail.com>
2023-03-17 16:18:45 -0300

    update header

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { logOut } from "../redux/userSlice";
 import { toggleDarkMode } from "../redux/darkModeSlice";
+import TweetModal from "./TweetModal";
 
 function Header() {
   const user = useSelector((state) => state.user);
@@ -13,13 +14,11 @@ function Header() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
   const handleToggleDarkMode = () => {
     dispatch(toggleDarkMode());
-  }
+  };
 
   return (
-<<<<<<< HEAD
     <>
       <div style={{ position: "absolute" }}>
         <TweetModal handleClose={handleClose} show={show} />
@@ -39,47 +38,11 @@ function Header() {
               className="d-flex justify-content-center"
               style={{ backgroundColor: "#f01d1d00", border: "none" }}
             >
-=======
-    <div className={`sticky-top ${isDarkModeEnabled ? 'darkMode' : 'whiteMode'} `}>
-      {showOptions && (
-        <div
-          style={{
-            position: "absolute",
-            zIndex: "100",
-            left: "15rem",
-            bottom: "10px",
-          }}
-        >
-          <button
-            onClick={() => dispatch(logOut())}
-            className="d-flex justify-content-center"
-            style={{ backgroundColor: "#f01d1d00", border: "none" }}
-          >
-            <img
-              style={{ width: "50px", backgroundBlendMode: "multiply" }}
-              src="https://i.ibb.co/9HjCnvx/removal-ai-tmp-641209035fdb3.png"
-              alt="user_icon"
-            />
-          </button>
-
-        </div>
-      )}
-      <div
-        className="absolute sticky-top d-grid justify-content-end py-3 pt-4 bg-white"
-        style={{ height: "100vh", zIndex: "500" }}
-      >
-        {/*<!-- Nav top --> */}
-        <div className="d-flex flex-column justify-content-between align-items-end">
-          <div className="d-flex flex-column align-items-center gap-4">
-            <Link to={"/"}>
-              {/* <!--       Tweeter Anchor --> */}
->>>>>>> 9a0049e3b32be221c8843d23ad523876f4f088d5
               <img
                 style={{ width: "50px", backgroundBlendMode: "multiply" }}
                 src="https://i.ibb.co/9HjCnvx/removal-ai-tmp-641209035fdb3.png"
                 alt="user_icon"
               />
-<<<<<<< HEAD
             </button>
           </div>
         )}
@@ -137,29 +100,6 @@ function Header() {
             >
               <div style={{ fontSize: "4rem", color: "#1d9bf0" }}>...</div>
             </button>
-=======
-            </Link>
-            {/* <!--       New Tweet Anchor --> */}
-            <Link to={"/"}>
-              <img
-                style={{ width: "100px" }}
-                src="https://beconnected.esafety.gov.au/pluginfile.php/69203/mod_resource/content/1/t26_c6_a4_p2.png"
-                alt="user_icon"
-              />
-            </Link>
-             {/* <!-- Dark Mode --> */}
-             <button className="btn" onClick={handleToggleDarkMode} style={{ display: 'none'}}>
-             <img
-                style={{ width: "60px", display: 'none' }}
-                src="https://cdn-icons-png.flaticon.com/512/6714/6714978.png"
-                alt="user_icon"
-                
-                
-              />
-
-             </button>
-           
->>>>>>> 9a0049e3b32be221c8843d23ad523876f4f088d5
           </div>
         </div>
       </div>
