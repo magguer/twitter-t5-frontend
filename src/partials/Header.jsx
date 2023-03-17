d29e704361fd3bcfaa39ce119c8d227206f414fa
Emi <emi.lng711@gmail.com>
2023-03-17 16:14:17 -0300

    TweetModal

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -2,93 +2,102 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { logOut } from "../redux/userSlice";
+import TweetModal from "./TweetModal";
 
 function Header() {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [showOptions, setShowOptions] = useState(false);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <div className="sticky-top">
-      {showOptions && (
-        <div
-          style={{
-            position: "absolute",
-            zIndex: "100",
-            left: "15rem",
-            bottom: "10px",
-          }}
-        >
-          <button
-            onClick={() => dispatch(logOut())}
-            className="d-flex justify-content-center"
-            style={{ backgroundColor: "#f01d1d00", border: "none" }}
+    <>
+      <div style={{ position: "absolute" }}>
+        <TweetModal handleClose={handleClose} show={show} />
+      </div>
+      <div className="sticky-top">
+        {showOptions && (
+          <div
+            style={{
+              position: "absolute",
+              zIndex: "100",
+              left: "15rem",
+              bottom: "10px",
+            }}
           >
-            <img
-              style={{ width: "50px", backgroundBlendMode: "multiply" }}
-              src="https://i.ibb.co/9HjCnvx/removal-ai-tmp-641209035fdb3.png"
-              alt="user_icon"
-            />
-          </button>
-        </div>
-      )}
-      <div
-        className="absolute sticky-top d-grid justify-content-end py-3 pt-4 bg-white"
-        style={{ height: "100vh", zIndex: "500" }}
-      >
-        {/*<!-- Nav top --> */}
-        <div className="d-flex flex-column justify-content-between align-items-end">
-          <div className="d-flex flex-column align-items-center gap-4">
-            <Link to={"/"}>
-              {/* <!--       Tweeter Anchor --> */}
+            <button
+              onClick={() => dispatch(logOut())}
+              className="d-flex justify-content-center"
+              style={{ backgroundColor: "#f01d1d00", border: "none" }}
+            >
               <img
-                style={{ width: "30px" }}
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/768px-Twitter-logo.svg.png"
-                alt="logo_twitter"
-              />
-            </Link>
-            {/*<!--       Home Anchor --> */}
-            <Link to={"/"}>
-              <img
-                style={{ width: "40px" }}
-                src="https://www.shareicon.net/data/512x512/2017/05/26/886463_home_512x512.png"
-                alt="home_icon"
-              />
-            </Link>
-            {/*<!--       User Profile Anchor --> */}
-            <Link to={`/${user.userName}`}>
-              {" "}
-              {/*Originalmente iba a: /:username/<%= user.username */}
-              <img
-                style={{ width: "40px" }}
-                src="https://icons-for-free.com/iconfiles/png/512/human+person+user+icon-1320196276306824343.png"
+                style={{ width: "50px", backgroundBlendMode: "multiply" }}
+                src="https://i.ibb.co/9HjCnvx/removal-ai-tmp-641209035fdb3.png"
                 alt="user_icon"
               />
-            </Link>
-            {/* <!--       New Tweet Anchor --> */}
-            <Link to={"/"}>
-              <img
-                style={{ width: "100px" }}
-                src="https://beconnected.esafety.gov.au/pluginfile.php/69203/mod_resource/content/1/t26_c6_a4_p2.png"
-                alt="user_icon"
-              />
-            </Link>
+            </button>
+          </div>
+        )}
+        <div
+          className="absolute sticky-top d-grid justify-content-end py-3 pt-4 bg-white"
+          style={{ height: "100vh", zIndex: "500" }}
+        >
+          {/*<!-- Nav top --> */}
+          <div className="d-flex flex-column justify-content-between align-items-end">
+            <div className="d-flex flex-column align-items-center gap-4">
+              <Link to={"/"}>
+                {/* <!--       Tweeter Anchor --> */}
+                <img
+                  style={{ width: "30px" }}
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/768px-Twitter-logo.svg.png"
+                  alt="logo_twitter"
+                />
+              </Link>
+              {/*<!--       Home Anchor --> */}
+              <Link to={"/"}>
+                <img
+                  style={{ width: "40px" }}
+                  src="https://www.shareicon.net/data/512x512/2017/05/26/886463_home_512x512.png"
+                  alt="home_icon"
+                />
+              </Link>
+              {/*<!--       User Profile Anchor --> */}
+              <Link to={`/${user.userName}`}>
+                {" "}
+                {/*Originalmente iba a: /:username/<%= user.username */}
+                <img
+                  style={{ width: "40px" }}
+                  src="https://icons-for-free.com/iconfiles/png/512/human+person+user+icon-1320196276306824343.png"
+                  alt="user_icon"
+                />
+              </Link>
+              {/* <!--       New Tweet Anchor --> */}
+              <button className="btn" onClick={handleShow}>
+                <img
+                  style={{ width: "100px" }}
+                  src="https://beconnected.esafety.gov.au/pluginfile.php/69203/mod_resource/content/1/t26_c6_a4_p2.png"
+                  alt="tweet_icon"
+                />
+              </button>
+            </div>
+          </div>
+          {/*<!--   Nav logout --> */}
+          <div className="d-flex flex-column justify-content-end">
+            {/*<!--     LogOut Button -->*/}
+            <button
+              /* onClick={() => setShowOptions(!showOptions)} */
+              onClick={() => dispatch(logOut())}
+              className="d-flex justify-content-center"
+              style={{ backgroundColor: "#f01d1d00", border: "none" }}
+            >
+              <div style={{ fontSize: "4rem", color: "#1d9bf0" }}>...</div>
+            </button>
           </div>
         </div>
-        {/*<!--   Nav logout --> */}
-        <div className="d-flex flex-column justify-content-end">
-          {/*<!--     LogOut Button -->*/}
-          <button
-            /* onClick={() => setShowOptions(!showOptions)} */
-            onClick={() => dispatch(logOut())}
-            className="d-flex justify-content-center"
-            style={{ backgroundColor: "#f01d1d00", border: "none" }}
-          >
-            <div style={{ fontSize: "4rem", color: "#1d9bf0" }}>...</div>
-          </button>
-        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/partials/TweetModal.jsx
@@ -0,0 +1,100 @@
+import axios from "axios";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useSelector } from "react-redux";
+import { actualize } from "../redux/resetSlice";
+
+function TweetModal({ show, handleClose }) {
+  const [tweet, setTweet] = useState("");
+  const user = useSelector((state) => state.user);
+
+  const handleTweet = async (e) => {
+    if (tweet !== "") {
+      dispatchEvent(actualize());
+      e.preventDefault();
+      await axios({
+        headers: {
+          Authorization: `Bearer ${user.userToken}`,
+        },
+        method: "POST",
+        url: `${process.env.REACT_APP_API_URL}/tweets/`,
+        data: { tweet: tweet },
+      });
+    }
+  };
+
+  return (
+    <Modal
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      show={show}
+      handleClose={handleClose}
+      onHide={handleClose}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <h2 className="text-center">Post Your Tweet 😉 </h2>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form>
+          <div className="d-flex">
+            <div className="me-2">
+              <img
+                style={{ width: "4rem" }}
+                className="figure-img img-fluid rounded-pill align-self-start"
+                alt="images"
+                src={
+                  user.userImage.includes("http")
+                    ? user.userImage
+                    : `${process.env.REACT_APP_API_URL}/img/${user.userImage}`
+                }
+              />
+            </div>
+            <div className="w-100">
+              <textarea
+                className="form-control"
+                placeholder="What's happening?"
+                name="newTweet"
+                id="newTweet"
+                style={{ height: "100px" }}
+                value={tweet}
+                onChange={(e) => setTweet(e.target.value)}
+              />
+            </div>
+          </div>
+          <hr />
+          <div className="d-flex justify-content-end">
+            {/* <Button
+              className="btn text-light ms-2"
+              style={{
+                borderRadius: "45px",
+                backgroundColor: "#1d9bf0",
+                height: "40px",
+              }}
+              variant="secondary"
+              onClick={handleClose}
+            >
+              Close
+            </Button> */}
+            <Button
+              className="btn text-light"
+              type="submit"
+              style={{
+                borderRadius: "45px",
+                backgroundColor: "#1d9bf0",
+                height: "40px",
+              }}
+              onClick={handleTweet}
+            >
+              Tweet
+            </Button>
+          </div>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default TweetModal;
